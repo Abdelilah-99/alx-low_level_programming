b9ea81b310fe2e6cf1c7863d31054e3b17e47719
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-17 16:29:23 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - hex
+ * Return: Returns 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 0; i <= 15; i++)
+	{
+	if (i < 10)
+	{
+	putchar(i + '0');
+	}
+	else
+	{
+	putchar(i - 10 + 'a');
+	}
+	putchar('\n');
+	}
+	return (0);
+}
