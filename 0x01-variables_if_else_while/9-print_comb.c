0fc8b76b37796ecf0d3f766d28acf07baea3b5aa
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-17 17:05:11 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - not big deal
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+	putchar(i + '0');
+	if (i == 9)
+	break;
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
