dd1478841d9db29dd122d162c7f07b821a512498
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-17 18:17:59 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - im happy hamdullah
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int i, j, x, y;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = 0; j <= 9; j++)
+	{
+	for (x = 0; x <= 9; x++)
+	{
+	for (y = i + 1; y <= 9; y++)
+	{
+	putchar(i + '0');
+	putchar(j + '0');
+	putchar(' ');
+	putchar(x + '0');
+	putchar(y + '0');
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
