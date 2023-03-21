021607db55f0af85add7a0eece7ee021f07fe7d8
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-21 15:37:16 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * _islower - mu
- * @c: charactere
+ * @i: charactere
  * Return: 1 or 0
  */
-int _islower(int c)
+int _islower(int i)
 {
 	if (i >= 'a' && i <= 'z')
 		return (1);
