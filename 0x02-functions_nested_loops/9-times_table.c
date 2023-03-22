24d195c9deecea4dc701b186e34929a5d819983f
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-22 05:16:36 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * times_table - tu
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			s = i * j;
+			if (i <= 9)
+				_putchar(' ');
+			else
+				_putchar((s / 10) + '0');
+				_putchar((s % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
