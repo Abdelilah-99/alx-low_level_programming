73483a8e35153b40943af602ee8882a1dfccbb13
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-23 14:50:08 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,21 +10,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("FizzBuzz");
-			continue;
-		}
-		if (i % 3 == 0)
-		{
+		else if (i % 3 == 0)
 			printf("Fizz");
-			continue;
-		}
-		if (i % 5 == 0)
-		{
+		else if (i % 5 == 0)
 			printf("Buzz");
-			continue;
-		}
+		else
 			printf("%d", i);
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
