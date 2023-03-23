643d7ef5de0d20c6e85570a432e64d916e46962d
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-23 09:22:59 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - ligne
+ * @n: nombre of lines
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+		if (n <= 0)
+			putchar('\n');
+		else
+			putchar('_');
+			putchar('\n');
+}
