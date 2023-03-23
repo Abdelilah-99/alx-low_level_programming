c91b6924a269d9ddccf00cc77062a969fd0f8a29
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-23 06:51:36 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - that function used for know is the digit a uppercase or not
+ * @c: if c a uppercase charactere the return will be 1 if not return 0
+ * Return: Returns 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else 
+		return (0);
+	return (0);
+}
