74a48b8194e5b1c6d6e99d7b95186904d99b8ebb
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-23 07:09:41 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - here we will return resul of a*b
+ * @a: represent the 1st value
+ * @b: represent 2sd value
+ * Return: Returns a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
