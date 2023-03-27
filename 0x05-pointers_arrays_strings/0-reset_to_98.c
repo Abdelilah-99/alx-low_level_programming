0f95e4f1281f370bd717e912762adbd7c891ae03
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-27 08:45:08 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - when the .. is calling n will be the
+ * pointer and will point the value to the vari
+ * @n: i think
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
