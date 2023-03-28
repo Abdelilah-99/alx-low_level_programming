86f70fe736720655a7ec242309974c72e4d69401
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-28 06:56:25 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *_strcpy - cpu
+ * @dest: mk
+ * @src: mki
+ * Return: Returns dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ int _strlen(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
