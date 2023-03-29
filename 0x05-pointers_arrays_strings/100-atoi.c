5eb703b0306ff96390977a37a9edbeab1b551149
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-28 17:50:26 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _atoi - that's make life easy with c simply it ignore every thing
+ * not a integer but i ve made an exception for my baby - cause
+ * she is everything to my code
+ * @s: i've made an array without that i know
+ * Return: Returns result
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int signe = 1;
+	int check = 0;
+	int result = 0;
+
+	while (s[i])
+	{
+		if (s[i] == '-')
+		{
+			signe *= -1;
+		}
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			check = 1;
+			result = (result * 10) + (s[i] - '0');
+			i++;
+		}
+		if (check == 1)
+		{
+			break;
+		}
+		i++;
+	}
+	result *= signe;
+	return (result);
+}
