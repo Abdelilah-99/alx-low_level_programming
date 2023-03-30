f1c9408082908a75f770d717faafb950eb475b10
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-30 16:45:23 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,26 +6,26 @@
  */
 char *cap_string(char *k)
 {
-	int i = 0;
-	int test = 0;
+int i = 0;
+int test = 0;
 
-	while (k[i] != '\0')
-	{
-		if (i == 0)
-			k[i] = k[i] - 32;
-		if (test == 1 && k[i] >= 'a' && k[i] <= 'z')
-		{
-			k[i] = k[i] - 32;
-		}
-		if (k[i] == ' ' || k[i] == ';' || k[i] == '.' || k[i] == ',' ||
-				k[i] == '!' || k[i] == '{' || k[i] == '}' || k[i] == '?' || k[i] == '"' ||
-				k[i] == ')' || k[i] == '(' || k[i] == 9 || k[i] == 10)
-		{
-			test = 1;
-		}
-		else
-			test = 0;
-		i++;
-	}
-	return (k);
+while (k[i] != '\0')
+{
+if (i == 0)
+k[i] = k[i] - 32;
+if (test == 1 && k[i] >= 'a' && k[i] <= 'z')
+{
+k[i] = k[i] - 32;
+}
+if (k[i] == ' ' || k[i] == ';' || k[i] == '.' || k[i] == ',' ||
+k[i] == '!' || k[i] == '{' || k[i] == '}' || k[i] == '?' || k[i] == '"' ||
+k[i] == ')' || k[i] == '(' || k[i] == 9 || k[i] == 10)
+{
+test = 1;
+}
+else
+test = 0;
+i++;
+}
+return (k);
 }
