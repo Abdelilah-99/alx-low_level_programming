65ffb49760da200ef366429019a8e791f2dc7b94
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-30 10:06:58 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * *cap_string - i ve did it
+ * @k: i can play
+ * Return: Returns k
+ */
+char *cap_string(char *k)
+{
+int i = 0;
+int test = 0;
+
+while (k[i] != '\0')
+{
+if (test == 1 && k[i] >= 'a' && k[i] <= 'z')
+{
+k[i] = k[i] - 32;
+}
+if (k[i] == ' ' || k[i] == ';' || k[i] == '.' || k[i] == ',' ||
+k[i] == '!' || k[i] == '{' || k[i] == '}' || k[i] == '?' || k[i] == '"' ||
+k[i] == ')' || k[i] == '(' || k[i] == '\t' || k[i] == '\n')
+{
+test = 1;
+}
+else
+test = 0;
+i++;
+}
+return (k);
+}
