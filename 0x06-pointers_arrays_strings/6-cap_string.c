34492f5ec82853ff9dba1e43365691c56a87c69f
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-30 16:25:32 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,23 +6,24 @@
  */
 char *cap_string(char *k)
 {
-int i = 0;
-int test = 0;
-int t[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i = 0;
+	int test = 0;
 
-while (k[i] != '\0')
-{
-if (test == 1 && k[i] >= 'a' && k[i] <= 'z')
-{
-k[i] = k[i] - 32;
-}
-if (k[i] == *t)
-{
-test = 1;
-}
-else
-test = 0;
-i++;
-}
-return (k);
+	while (k[i] != '\0')
+	{
+		if (test == 1 && k[i] >= 'a' && k[i] <= 'z')
+		{
+			k[i] = k[i] - 32;
+		}
+		if (k[i] == ' ' || k[i] == ';' || k[i] == '.' || k[i] == ',' ||
+				k[i] == '!' || k[i] == '{' || k[i] == '}' || k[i] == '?' || k[i] == '"' ||
+				k[i] == ')' || k[i] == '(' || k[i] == 9 || k[i] == 10)
+		{
+			test = 1;
+		}
+		else
+			test = 0;
+		i++;
+	}
+	return (k);
 }
