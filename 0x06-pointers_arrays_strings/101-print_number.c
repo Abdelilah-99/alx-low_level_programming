7c309bf79c85c8268bddff715a7559d2011aa957
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-03-31 18:53:06 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_number - ko
+ * @n: jjj
+ */
+void print_number(int n)
+{
+	int *p;
+
+	p = &n;
+	if (n < 0)
+	{
+		putchar('-');
+		*p *= -1;
+	}
+	int test = 1;
+
+	while (*p / test >= 10)
+	{
+		test *= 10;
+	}
+	while (test > 0)
+	{
+		putchar(*p / test + 48);
+		*p %= test;
+		test = test / 10;
+	}
+}
+
