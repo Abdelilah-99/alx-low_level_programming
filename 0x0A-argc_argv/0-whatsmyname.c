6e42b95c790427e8e1381876a38af56406f7de10
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-04-08 08:04:02 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - containing 2 parameter argc and argv by giving argv[0]
+ * the output is the name of the programme
+ * @argc: go to resource
+ * @argv: respiu
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
