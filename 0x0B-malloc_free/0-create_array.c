476562df15005f2b2e30bf0d86e1e1d7b4993a82
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-04-11 18:28:18 +0000

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_array - had lprogramme kaychuf size wach equal l 0
+ * muraha katchuuf wach kayan espace f la memoire ram
+ * @size: ila kan 3andna ktarr men charachter kan7assbu ch7al
+ * kayan mnuu w nadarbuuh f la taill ta3u bach ndiru espace
+ * lii kafi l chnuuu bghina ndiir f la memoire ram
+ * @c: chnuu kayan f T f l7alatt d i ghadi y installa f c
+ *
+ * Return: T and is c and c is H so it return Size x H
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (NULL);
+	char *T = malloc(sizeof(*T) * size);
+
+	if (T == NULL)
+		return (NULL);
+	unsigned int i = 0;
+
+	while (i < size)
+	{
+		T[i] = c;
+		i++;
+	}
+	return (T);
+}
