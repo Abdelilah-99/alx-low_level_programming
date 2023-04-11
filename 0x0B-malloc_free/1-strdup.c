3941a5de87236a8848f0516e98e98378b14b4995
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-04-11 21:53:02 +0000

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strdup - ma3arfch had lfunction chnu kadiri
+ * wlkn lii faham b had l programme lii khdmt w bdaak
+ * chii lii 3tawni drt programme lii yraja3 string
+ * lii muhim machi hada lii drtt huwa khadiit 9ed mabaghi d memoire
+ * b malloc function fhad string 3ndi 7 ta3 les daak chii
+ * madartt f memoire
+ * @str: hada huwa string lii jabtuu w raditu w ma3rfch 3lch
+ * Return: ila kan oversize raah yrajja3 null hadii menn khassyt
+ * d malloc zedt diik fnct 9bel return talya la7a9ach 3ndi lbreak
+ * ghi fach ykuun string wssl l null ya3ni atab9ali
+ * case khawya f srting li hya 7 w tmmk drt val null
+ */
+char *_strdup(char *str)
+{
+	char *T;
+	int i = 0;
+
+	T = malloc(sizeof(*T) * *str);
+	if (T == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		T[i] = str[i];
+		i++;
+	}
+	T[i] = '\0';
+	return (T);
+}
