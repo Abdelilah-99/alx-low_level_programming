db71a028d71fe2c4c726c90add34b0374989f08b
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-04-11 02:22:11 +0000

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,29 +9,29 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents, coins;
+	int DH, CO;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[1]);
-	if (cents < 0)
+	DH = atoi(argv[1]);
+	if (DH < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	coins = 0;
-	coins += cents / 25;
-	cents %= 25;
-	coins += cents / 10;
-	cents %= 10;
-	coins += cents / 5;
-	cents %= 5;
-	coins += cents / 2;
-	cents %= 2;
-	coins += cents;
-	printf("%d\n", coins);
+	CO = 0;
+	CO += DH / 25;
+	DH %= 25;
+	CO += DH / 10;
+	DH %= 10;
+	CO += DH / 5;
+	DH %= 5;
+	CO += DH / 2;
+	DH %= 2;
+	CO += DH;
+	printf("%d\n", CO);
 	return (0);
 }
