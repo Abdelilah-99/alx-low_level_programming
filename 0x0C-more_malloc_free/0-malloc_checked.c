7ae8c1f95df5e4c1dcfa66516e1495402c092c0f
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-04-13 16:29:44 +0000

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,17 +2,15 @@
 
 /**
  * malloc_checked - had lfunction ghady treturner les adresse ta3
- * 
  * malloc(b)
- * 
  * @b: size w sf
- * 
  * Return: adresse lii ghady ykuun f ptr
 */
-void *malloc_checked(unsigned int b) {
-    void *ptr = malloc(b);
+void *malloc_checked(unsigned int b)
+{
+void *ptr = malloc(b);
 
-    if (ptr == NULL)
-        exit(98);
-    return (ptr);
+if (ptr == NULL)
+	exit(98);
+return (ptr);
 }
