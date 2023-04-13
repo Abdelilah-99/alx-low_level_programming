c25306628552e4994a8ba139a8b2d0d6d5997356
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-04-13 01:36:22 +0000

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * free_grid - hna b7aal ilaa l9iina chii merrat chii fail
+ * w kaan andna 2D bach nvidiw la memoire
+ * @grid: 2d array
+ * @height: row
+*/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free (grid[i]);
+	free (grid);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,3 +8,4 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int **alloc_grid(int width, int height);
 char *argstostr(int ac, char **av);
+void free_grid(int **grid, int height);
