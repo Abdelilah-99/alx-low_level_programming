268ee1f5723538ca60917fd898eadcd663c04581
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-04-14 01:22:53 +0000

    3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * array_range - here i just need to arrang num from min to max
+ * @min: the min num
+ * @max: the max num
+ * Return: T
+*/
+int *array_range(int min, int max)
+{
+	int *T;
+	int size, i;
+
+	size = max - min + 1;
+	if (min > max)
+	return (NULL);
+	T = malloc(sizeof(int) * size);
+	for (i = 0; i < size; i++)
+	{
+		T[i] = min + i;
+	}
+	return (T);
+}
