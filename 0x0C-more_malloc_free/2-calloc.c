9f5f9f9af311ba1c8da00ce97cd75b0c239d7471
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-04-14 00:59:44 +0000

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _calloc - ghady ndiru mem w initializiwha b 0
+ * malloc katkuun f mem ta3ha l garbedge
+ * @nmemb: ghii ch7aal b merra
+ * @size: had size normallement huwa size char
+ * Return: p
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	return (NULL);
+	p = malloc(sizeof(nmemb * size));
+	if (p == 0)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
