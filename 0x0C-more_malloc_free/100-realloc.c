eeafce96ea5a5dd7d3833b8477a8d0de8e81dd75
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-04-14 15:07:18 +0000

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -46,7 +46,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 		return (new_ptr);
 	}
-	if (new_ptr == ptr)
+	if (old_size == new_size)
 		return (ptr);
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
@@ -57,4 +57,46 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		_memcpy(new_ptr, ptr, new_size);
 	free(ptr);
 	return (new_ptr);
+}
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *p;
+    int i;
+
+    p = malloc(sizeof(char) * 10);
+    p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
+    i = 0;
+    while (i < 98)
+    {
+        p[i++] = 98;
+    }
+    simple_print_buffer(p, 98);
+    free(p);
+    return (0);
 }
