3021418da787d31e9398e8150f967ca425d545c3
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-04-28 00:08:42 +0100

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * _len - func cal the len
+ * @s: pointer to str
+ * Return: len
+*/
+unsigned int _len(char *s)
+{
+	unsigned int len = 0;
+
+	for (; s[len]; len++)
+		;
+	return (len);
+}
+/**
+ * add_node - adding new node to the begining
+ * @head: pointer to pointer head -> head -> &list_t
+ * @str: string
+ * Return: new_node
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = _len(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
