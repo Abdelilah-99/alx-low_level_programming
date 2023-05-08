f7b7da1edaa8ae5b04c445a21004feb16764fb37
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-05-08 19:43:30 +0100

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * read_textfile - func can read file
+ * @filename: kadawaz l arg 1
+ * @letters: nmbr d char
+ * Return: 0 if there any error or r
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	ssize_t r, w;
+	char *buff;
+
+	if (!filename)
+		return (0);
+	fp = fopen(filename, O_RDONLY);
+		if (!fp)
+			return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+	{
+		fclose(fp);
+		return (0);
+	}
+	r = fread(buff, sizeof(char), letters, fp);
+	if (!r || ferror(fp))
+	{
+		fclose(fp);
+		free(buff);
+		return (0);
+	}
+	w = write(STDOUT_FILENO, buff, r);
+	if (r != w)
+	{
+		fclose(fp);
+		free(buff);
+		return (0);
+	}
+	fclose(fp);
+	free(buff);
+	return (r);
+}
