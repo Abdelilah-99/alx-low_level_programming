078d960e7234bdd05de2e72bef42f573ec043d09
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-05-09 01:48:57 +0100

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,8 +12,6 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	if (!text_content)
-		text_content = "";
 	for (; text_content[i]; i++)
 		;
 	fp = open(filename, O_WRONLY | O_APPEND);
