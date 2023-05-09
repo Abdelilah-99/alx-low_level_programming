39313d437f68723cf501087023685929ee4c51e5
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-05-09 01:37:31 +0100

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,18 @@
 #include "main.h"
 /**
+ * _strlen - cal the len
+ * @str: string
+ * Return: i
+*/
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+/**
  * append_text_to_file - add a text to a file that hold a text
  * but with no removing or editing the text
  * @filename: larg 1
@@ -8,21 +21,18 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	FILE *fp;
-	unsigned int i = 0;
+	int *fp;
+	ssize_t j;
 
 	if (!filename)
 		return (-1);
-	fp = fopen(filename, "a");
-	for (; text_content[i]; i++)
-		;
-	if (text_content == NULL)
-		return (0);
-	if (fwrite(text_content, sizeof(char), i, fp) != i)
-	{
-		fclose(fp);
+	fp = open(filename, O_RDONLY | O_APPEND);
+	if (fp == -1)
 		return (-1);
-	}
+	if (text_content != NULL)
+		j = write(fp, text_content, _strlen(text_content));
 	fclose(fp);
+	if (j == -1)
+		return (-1);
 	return (1);
 }
