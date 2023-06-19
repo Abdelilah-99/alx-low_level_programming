bfd0608f42c05be1fe7c400b545e6ce0133f96d0
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-06-19 21:49:30 +0100

    test

---
+++ 0x18-dynamic_libraries/test/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - mu
+ * @i: charactere
+ * Return: 1 or 0
+ */
+int _islower(int i)
+{
+	if (i >= 'a' && i <= 'z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x18-dynamic_libraries/test/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - fhfhf
+ * @n: lot
+ * Return: -n or n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
