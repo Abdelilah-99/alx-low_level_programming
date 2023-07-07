ca32eb4ed675e834b4a9efd6d53b7b2ba7fea01b
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-07-07 23:23:52 +0100

    schr

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *item;
 	unsigned long int idx;
 
-	if (!ht || !key || *key == '\n' || !ht->array || ht->size == 0)
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+		key == NULL || strlen(key) == 0)
 		return (NULL);
 	idx = key_index((const unsigned char *)key, ht->size);
 	item = ht->array[idx];
