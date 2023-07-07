ae7920f879aee36a4351980fd6fbb91b86ca8d28
Abdelilah-99 <bouchikhiabdelilah0@gmail.com>
2023-07-07 23:09:50 +0100

    search for a value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - searche for item
+ * @ht: hash table
+ * @key: key
+ * Return: item->value or null if didn't find any thing
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *item;
+	unsigned long int idx;
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	item = ht->array[idx];
+	if (item)
+	{
+		if (strcmp(item->key, key) == 0)
+			return (item->value);
+	}
+	return (NULL);
+}
